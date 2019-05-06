6738a3bcd6d39e14a73a4eff31f6ea7fbcac8924
Shaun W Colley <swc7838@gmail.com>
2019-05-06 15:40:08 -0500

    hiking app is finished

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
 const saltRounds = 10
 const app = express()
 const PORT = process.env.PORT || 8080
@@ -9,6 +10,24 @@ const PORT = process.env.PORT || 8080
 app.use(cors())
 app.use(bodyParser.json())
 models = require('./models')
+let userId = ''
+
+function authenticate(req,res,next) {
+  let headers = req.headers["authorization"]
+  let token = headers.split(' ')[1]
+  jwt.verify(token, 'twasbrillig', (err, decoded) => {
+    if(decoded) {
+      if(decoded.userId) {
+        userId = decoded.userId
+        next()
+      } else {
+        res.status(401).json({message: 'Invalid Token'})
+      }
+    } else {
+      res.status(401).json({message: 'Invalid Token', err: err})
+    }
+  })
+}
 
 app.get('/api',(req,res) => {
   res.json({message: "hiking location api"})
@@ -44,6 +63,10 @@ app.post('/register', (req,res) => {
   })
 })
 
+app.get('/hello', authenticate, (req,res) => {
+  res.send('hello Jabb')
+})
+
 app.post('/login', (req,res) => {
   let userName = req.body.userName
   models.User.findAll({
@@ -57,10 +80,16 @@ app.post('/login', (req,res) => {
     else {
       bcrypt.compare(req.body.pass, user[0].pass, function(err, response) {
         if(response){
-          res.json({success:true, message: 'User Logged In.', userId: user[0].id})
+          jwt.sign({ userId: user[0].id}, 'twasbrillig', function(error, token) {
+            if(token){
+              res.json({success:true, message: 'User Logged In.', token: token, userId: user[0].id})
+            } else {
+              res.status(500).json({message: 'Unable to generate token', error: error})
+            }
+          })
         }
         else {
-          res.json({sucess:false, message: 'Invalid Password.'})
+          res.json({sucess:false, message: 'Invalid Password.', err: err})
         }
       })
     }
@@ -69,14 +98,19 @@ app.post('/login', (req,res) => {
 
 app.get('/view-locations/user-id/:id', (req,res) => {
   let id = req.params.id
-  res.json({message: 'Howdy'})
+  models.Coordinate.findAll({
+    where: {
+      userId: id
+    }
+  }).then((locations) => {
+    res.json({locations})
+  })
 })
 
-app.post('/location/user-id/:id', (req,res) => {
-  let id = req.params.id
+app.post('/location-save', authenticate, (req,res) => {
+  let id = userId
   let lat = req.body.lat
   let long = req.body.long
-
   let location = models.Coordinate.build({
     userId: id,
     lat: lat,
